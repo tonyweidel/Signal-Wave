50820600caf9118d9c1769efbfd27efc584d7fe8
Tony Weidel <8aweidel@stpatrick.org>
2017-09-12 14:41:43 -0500

    Its just the same thing 4 times

--- signal.js
+++ signal.js
@@ -13,7 +13,36 @@ let signal = function(array){
 
   function shiftX(value){
     for(a=0; a<numberOfPoints; a++){
-
+        let tempx = P1.getX();
+        let tempy = P1.getY();
+        tempx += value;
+        let tempPoint = new Point(tempx, tempy);
     }
   }
+
+  function shiftY(value){
+     for(a=0; a<numberOfPoints; a++){
+         let tempx = P1.getX();
+         let tempy = P1.getY();
+         tempx += value;
+         let tempPoint = new Point(tempx, tempy);
+     }
+  }
+  function stretchX(value){
+      for(a=0; a<numberOfPoints; a++){
+        let tempx = P1.getX();
+        let tempy = P1.getY();
+        tempx *= value;
+        let tempPoint = new Point(tempx, tempy);
+      }
+    }
+
+    function stretchY(value){
+      for(a=0; a<numberOfPoints; a++){
+        let tempx = P1.getX();
+        let tempy = P1.getY();
+        tempy *= value;
+        let tempPoint = new Point(tempx, tempy);
+      }
+    }
 }
